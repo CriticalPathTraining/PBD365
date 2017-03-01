8386acaeb4e22c697f2981a877021378b6d4031b
Ted Pattison <ted@tedpattison.net>
2017-02-28 22:32:51 -0500

    updates

---
+++ Modules/06_CustomVisuals/Demo/barchart/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_996C8E45_0D05_4C52_B0C1_2C020A0F20DE = {
+        name: 'PBI_CV_996C8E45_0D05_4C52_B0C1_2C020A0F20DE',
+        displayName: 'My Barchart Visual',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.5.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.PBI_CV_996C8E45_0D05_4C52_B0C1_2C020A0F20DE.Visual(options),
+        custom: true
+    };
+}
