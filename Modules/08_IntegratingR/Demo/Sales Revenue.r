4c57fdff0f82988fcf6d65c2471de437926320ec
Ted Pattison <ted@tedpattison.net>
2017-03-01 12:35:26 -0500

    Updates

---
+++ Modules/08_IntegratingR/Demo/Sales Revenue.r
@@ -0,0 +1,8 @@
+hist(dataset$Age, 
+     main = "Customer Count by Age",
+     col="lightblue", 
+     border="black",
+     ylab="Customer Count", 
+     xlab="Customer Age",
+     xlim = c(18, 100),
+     breaks=20 )
