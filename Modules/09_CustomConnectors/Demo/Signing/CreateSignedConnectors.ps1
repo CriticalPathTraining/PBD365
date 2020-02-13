85b52020d5289a4ce88490dd60bf21d158588380
Ted Pattison <ted@tedpattison.net>
2020-02-13 10:28:32 -0500

    Updates

---
+++ Modules/09_CustomConnectors/Demo/Signing/CreateSignedConnectors.ps1
@@ -0,0 +1,12 @@
+﻿cls
+
+$input = "$PSScriptRoot\Connectors\TripPin.mez"
+
+$output = "$PSScriptRoot\Connectors\SignedTripPin.pqx"
+
+$certPath = "$PSScriptRoot\Cert\MyDataConnectorCert.pfx"
+$certPassword = "pass@word1"
+
+.\MakePQX.exe pack -mz $input -t $output
+
+.\MakePQX sign $output --certificate $certPath --password $certPassword.\MakePQX.exe verify $output
---
+++ Modules/09_CustomConnectors/Demo/Signing/CreateX509Cert.ps1
@@ -0,0 +1,35 @@
+﻿cls
+
+# create certificate in local certificate store
+$certName = "MyDataConnectorCert"
+
+$cert = New-SelfSignedCertificate -CertStoreLocation cert:\localmachine\my `
+                                  -Subject $certName `
+                                  -FriendlyName $certName `
+                                  -NotAfter (Get-Date).AddYears(2)
+
+# get certificate thumbprint                                  
+$thumbprint = $cert.Thumbprint
+
+# get certifcate path
+$certPath = 'cert:\localMachine\my\' + $thumbprint
+Write-Host "Certificate created at $certPath"
+
+# export private key in pfx file
+$password = "pass@word1"
+$securePassword = ConvertTo-SecureString -String $password -Force -AsPlainText
+$pfx = Export-PfxCertificate -cert $certPath -FilePath "$PSScriptRoot\Cert\$certName.pfx" -Password $securePassword
+
+
+# create text file with certificate info
+$output = New-Object System.Text.StringBuilder
+$result = $output.AppendLine('Thumbprint:');
+$result = $output.AppendLine($thumbprint);
+
+$output.ToString() | Out-File -FilePath "$PSScriptRoot\Cert\$certName.txt" -Encoding ascii
+&"NOTEPAD.EXE" "$PSScriptRoot\Cert\$certname.txt"
+
+Write-Host "Removing Certificate from User's Local Store"
+$result = Remove-Item $cert.PSPath
+
+Write-Host "All work has been completed"
